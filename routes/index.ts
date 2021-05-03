b981739f7a75baabee93e7b706a2b3f9591a7cc0
Vini <benvini8@gmail.com>
2021-05-03 13:57:40 +0300

    add route for specific product

--- routes/index.ts
+++ routes/index.ts
@@ -1,16 +1,24 @@
-import express from 'express';
-const products = require('../products.json');
+import express from "express";
+const products = require("../products.json");
+import { Product } from "./types";
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const start = Number(req.query.start);
   let end = Number(req.query.end);
-  
-  if (end - 1 > products.length) {    
-    end = products.length
-  };
-  
-  res.send(products.slice(start, end))
+
+  if (end - 1 > products.length) {
+    end = products.length;
+  }
+
+  res.send(products.slice(start, end));
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log("id", id);
+  const product = products.filter((product: Product) => product.id === id);
+  res.send(product[0]);
 });
 
 module.exports = router;
---
+++ routes/types.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    categoryName: string;
+    img: string;
+    id: number;
+    name: string;
+    isNatran: boolean;
+    isSugar: boolean;
+    manufacturerName: string;
+    isShumanRavuy: boolean;
+    price: number;
+}
